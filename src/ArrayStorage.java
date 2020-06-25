5c8065dc3186acff7d489b7c0a727a0fa5d72f3a
Dmytro Ivakhnenko <dim@iowa.usa.com>
2020-06-25 16:47:29 +0300

    Add realisation of ArrayStorage (HW1)

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +7,55 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = size() - 1; i >= 0; i--) {
+            storage[i] = null;
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[size()] = resume;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume resume = null;
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                resume = storage[i];
+                break; //full search skip because uuids are unique (see req)
+            }
+        }
+        return resume;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size(); i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                for (int j = i; j < size() ; j++) {     //shift array by one element
+                    storage[j] = storage[j + 1];
+                }
+                break; //full search skip because uuids are unique (see req)
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size()];
+        resumes = Arrays.copyOfRange(storage, 0, size());
+        return resumes;
     }
 
     int size() {
-        return 0;
+        Integer lastElementIndex = null;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break; //full search skip according to the way how data is stored in storage (see req)
+            } else {
+                lastElementIndex = i;
+            }
+        }
+        return (lastElementIndex != null) ? 1 + lastElementIndex : 0; //size should be zero if no elements were found
     }
 }
