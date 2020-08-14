431de9ddfa2537ed295653fd98775a4bdc954220
Dmytro Ivakhnenko <dim@iowa.usa.com>
2020-08-14 14:50:39 +0300

    Add logging to AbstractStorage

--- src/ru/javawebinar/basejava/storage/AbstractStorage.java
+++ src/ru/javawebinar/basejava/storage/AbstractStorage.java
@@ -5,37 +5,44 @@ import ru.javawebinar.basejava.exception.NonExistStorageException;
 import ru.javawebinar.basejava.model.Resume;
 
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public abstract class AbstractStorage<SK> implements Storage {
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     @Override
     public void save(Resume r) {
+        LOG.info("Save " + r);
         SK elementPointer = getNonExistElementPointer(r.getUuid());
         doSave(r, elementPointer);
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK elementPointer = getExistElementPointer(uuid);
         return doGet(elementPointer);
     }
 
     @Override
     public void update(Resume r) {
+        LOG.info("Update " + r);
         SK elementPointer = getExistElementPointer(r.getUuid());
         doUpdate(r, elementPointer);
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK elementPointer = getExistElementPointer(uuid);
         doDelete(elementPointer);
     }
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("GetAllSorted");
         return doGetAll().sorted().collect(Collectors.toList());
     }
 
@@ -56,6 +63,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getExistElementPointer(String uuid) {
         SK elementPointer = getElementPointer(uuid);
         if (!isElementFound(elementPointer)) {
+            LOG.warning("Resume " + uuid + " not exists" );
             throw new NonExistStorageException(uuid);
         }
         return elementPointer;
@@ -64,6 +72,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     private SK getNonExistElementPointer(String uuid) {
         SK elementPointer = getElementPointer(uuid);
         if (isElementFound(elementPointer)) {
+            LOG.warning("Resume " + uuid + " already exists" );
             throw new ExistStorageException(uuid);
         }
         return elementPointer;
