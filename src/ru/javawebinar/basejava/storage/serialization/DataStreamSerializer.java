dd8b63b2bb19bb605b25f034e4d557c62207d843
Dmytro Ivakhnenko <dim@iowa.usa.com>
2020-09-15 17:48:24 +0300

    Refactor DataStreamSerializer class(HW_10)

--- src/ru/javawebinar/basejava/storage/serialization/DataStreamSerializer.java
+++ src/ru/javawebinar/basejava/storage/serialization/DataStreamSerializer.java
@@ -1,142 +1,174 @@
 package ru.javawebinar.basejava.storage.serialization;
 
+import ru.javawebinar.basejava.exception.ResumeDeserializationException;
+import ru.javawebinar.basejava.exception.ResumeSerializationException;
 import ru.javawebinar.basejava.model.*;
 
 import java.io.*;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class DataStreamSerializer implements SerializationStrategy {
+    final static String NULL = "null";
+    final static DateTimeFormatter MONTH_YEAR_FORMATTER = DateTimeFormatter.ofPattern("MM/yyyy");
 
     @Override
     public void doWrite(Resume r, OutputStream os) throws IOException {
         try (DataOutputStream dos = new DataOutputStream(os)) {
-            dos.writeUTF(r.getUuid());
-            dos.writeUTF(r.getFullName());
+            writeString(dos, r.getUuid());
+            writeString(dos, r.getFullName());
+            writeList(dos, r.getContacts().entrySet(), this::writeContacts);
+            writeList(dos, r.getSections().entrySet(), this::writeSections);
+        }
+    }
 
-            Map<ContactType, String> contacts = r.getContacts();
-            dos.writeInt(contacts.size());
-            for (Map.Entry<ContactType, String> entry : contacts.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                dos.writeUTF(entry.getValue());
-            }
+    private void writeContacts(DataOutputStream d, Map.Entry<ContactType, String> e) {
+        writeString(d, e.getKey().name());
+        writeString(d, e.getValue());
+    }
 
-            Map<SectionType, AbstractSection> sections = r.getSections();
-            dos.writeInt(sections.size());
-            for (Map.Entry<SectionType, AbstractSection> entry : sections.entrySet()) {
-                dos.writeUTF(entry.getKey().name());
-                switch (entry.getKey()) {
-                    case PERSONAL:
-                    case OBJECTIVE:
-                        TextSection t = (TextSection) entry.getValue();
-                        dos.writeUTF(t.getText());
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        ListSection ls = (ListSection) entry.getValue();
-                        dos.writeInt(ls.getItems().size());
-                        for (String s : ls.getItems()) {
-                            dos.writeUTF(s);
-                        }
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        ExperienceSection es = (ExperienceSection) entry.getValue();
-                        dos.writeInt(es.getExperienceList().size());
-                        for (Experience e : es.getExperienceList()) {
-                            dos.writeUTF(e.getPlace().getName());
-                            if (e.getPlace().getHomepage() != null) {
-                                dos.writeUTF(e.getPlace().getHomepage());
-                            } else {
-                                dos.writeUTF("null");
-                            }
-                            dos.writeInt(e.getPositions().size());
-                            for (Experience.Position p : e.getPositions()) {
-                                dos.writeUTF(p.getPosition());
-                                if (p.getDescription() != null) {
-                                    dos.writeUTF(p.getDescription());
-                                } else {
-                                    dos.writeUTF("null");
-                                }
-                                dos.writeUTF(p.getStartDate().format(DateTimeFormatter.ofPattern("MM/yyyy")));
-                                dos.writeUTF(p.getEndDate().format(DateTimeFormatter.ofPattern("MM/yyyy")));
-                            }
-                        }
-                        break;
-                    default:
-                        throw new IllegalStateException("Unexpected value: " + entry.getKey());
-                }
-            }
+    private void writeSections(final DataOutputStream dos, final Map.Entry<SectionType, AbstractSection> e) {
+        writeString(dos, e.getKey().name());
+        switch (e.getKey()) {
+            case PERSONAL:
+            case OBJECTIVE:
+                writeString(dos, ((TextSection) e.getValue()).getText());
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                writeList(dos, ((ListSection) e.getValue()).getItems(), this::writeString);
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                writeList(dos, ((ExperienceSection) e.getValue()).getExperienceList(), this::writeExperience);
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + e.getKey());
+        }
+    }
+
+    private <T> void writeList(final DataOutputStream dos, final Collection<T> l, BiConsumer<DataOutputStream, T> sup) {
+        writeInt(dos, l.size());
+        l.forEach((e) -> sup.accept(dos, e));
+    }
+
+    private void writeExperience(final DataOutputStream dos, final Experience e) {
+        writeString(dos, e.getPlace().getName());
+        writeString(dos, Optional.ofNullable(e.getPlace().getHomepage()).orElse(NULL));
+        writeList(dos, e.getPositions(), this::writePosition);
+    }
+
+    private void writePosition(DataOutputStream dos, final Experience.Position p) {
+        writeString(dos, p.getPosition());
+        writeString(dos, Optional.ofNullable(p.getDescription()).orElse(NULL));
+        writeString(dos, p.getStartDate().format(MONTH_YEAR_FORMATTER));
+        writeString(dos, p.getEndDate().format(MONTH_YEAR_FORMATTER));
+    }
+
+    private void writeString(final DataOutputStream dos, final String s) {
+        try {
+            dos.writeUTF(s);
+        } catch (IOException e) {
+            throw new ResumeSerializationException("Error writing string: " + s, e);
+        }
+    }
+
+    private void writeInt(final DataOutputStream dos, final int i) {
+        try {
+            dos.writeInt(i);
+        } catch (IOException e) {
+            throw new ResumeSerializationException("Error writing int: " + i, e);
         }
     }
 
     @Override
     public Resume doRead(InputStream is) throws IOException {
         try (DataInputStream dis = new DataInputStream(is)) {
-            String uuid = dis.readUTF();
-            String fullName = dis.readUTF();
+            String uuid = readString(dis);
+            String fullName = readString(dis);
             Resume resume = new Resume(uuid, fullName);
-            int size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                resume.addContact(ContactType.valueOf(dis.readUTF()), dis.readUTF());
-            }
-
-            size = dis.readInt();
-            for (int i = 0; i < size; i++) {
-                SectionType st = SectionType.valueOf(dis.readUTF());
-                switch (st) {
-                    case PERSONAL:
-                    case OBJECTIVE:
-                        resume.addSection(st, new TextSection(dis.readUTF()));
-                        break;
-                    case ACHIEVEMENT:
-                    case QUALIFICATIONS:
-                        int listSize = dis.readInt();
-                        List<String> ls1 = new ArrayList<>();
-                        for (int j = 0; j < listSize; j++) {
-                            ls1.add(dis.readUTF());
-                        }
-                        resume.addSection(st, new ListSection(ls1));
-                        break;
-                    case EXPERIENCE:
-                    case EDUCATION:
-                        List<Experience> ls2 = null;
-                        List<Experience.Position> ls3 = null;
-                        listSize = dis.readInt();
-                        ls2 = new ArrayList<>(listSize);
-                        for (int j = 0; j < listSize; j++) {
-                            String name = dis.readUTF();
-                            String homepage = dis.readUTF();
-                            if (homepage.equals("null")) {
-                                homepage = null;
-                            }
-                            Link l = new Link(name, homepage);
-                            int listSize2 = dis.readInt();
-                            ls3 = new ArrayList<>(listSize2);
-                            for (int k = 0; k < listSize2; k++) {
-                                String position = dis.readUTF();
-                                String description = dis.readUTF();
-                                if (description.equals("null")) {
-                                    description = null;
-                                }
-                                String startDate = dis.readUTF();
-                                String endDate = dis.readUTF();
-                                Experience.Position pos = new Experience.Position(YearMonth.parse(startDate, DateTimeFormatter.ofPattern("MM/yyyy")), YearMonth.parse(endDate, DateTimeFormatter.ofPattern("MM/yyyy")), position, description);
-                                ls3.add(pos);
-                            }
-                            Experience e = new Experience(ls3, l);
-                            ls2.add(e);
-                        }
-                        ExperienceSection ex = new ExperienceSection(ls2);
-                        resume.addSection(st, ex);
-                        break;
-                }
-            }
+            readResumeBlock(dis, resume, this::readContacts);
+            readResumeBlock(dis, resume, this::readSections);
             return resume;
         }
     }
-}
 
+    private void readSections(DataInputStream dis, Resume resume) {
+        SectionType st = SectionType.valueOf(readString(dis));
+        switch (st) {
+            case PERSONAL:
+            case OBJECTIVE:
+                resume.addSection(st, new TextSection(readString(dis)));
+                break;
+            case ACHIEVEMENT:
+            case QUALIFICATIONS:
+                resume.addSection(st, new ListSection(readList(dis, this::readString)));
+                break;
+            case EXPERIENCE:
+            case EDUCATION:
+                resume.addSection(st, new ExperienceSection(readList(dis, this::readExperience)));
+                break;
+        }
+    }
+
+    private void readContacts(DataInputStream dis, Resume resume) {
+        resume.addContact(ContactType.valueOf(readString(dis)), readString(dis));
+    }
+
+    private void readResumeBlock(DataInputStream dis, Resume resume, BiConsumer<DataInputStream, Resume> consumer) {
+        int size = readInt(dis);
+        for (int i = 0; i < size; i++) {
+            consumer.accept(dis, resume);
+        }
+    }
+
+    private Experience readExperience(DataInputStream dis) {
+        String name = readString(dis);
+        String homepage = readNullString(dis);
+        Link l = new Link(name, homepage);
+        List<Experience.Position> lp = readList(dis, this::readPosition);
+        return new Experience(lp, l);
+    }
+
+    private Experience.Position readPosition(DataInputStream dis) {
+        String position = readString(dis);
+        String description = readNullString(dis);
+        String startDate = readString(dis);
+        String endDate = readString(dis);
+        return new Experience.Position(YearMonth.parse(startDate, MONTH_YEAR_FORMATTER), YearMonth.parse(endDate, MONTH_YEAR_FORMATTER), position, description);
+    }
+
+    private <T> List<T> readList(DataInputStream dis, Function<DataInputStream, T> function) {
+        int listSize = readInt(dis);
+        return Stream.generate(() -> function.apply(dis)).limit(listSize).collect(Collectors.toList());
+    }
+
+    private String readString(final DataInputStream dis) {
+        try {
+            return dis.readUTF();
+        } catch (IOException e) {
+            throw new ResumeDeserializationException("Error reading string value", e);
+        }
+    }
+
+    private String readNullString(DataInputStream dis) {
+        String s = readString(dis);
+        return NULL.equals(s) ? null : s;
+    }
+
+    private int readInt(final DataInputStream dis) {
+        try {
+            return dis.readInt();
+        } catch (IOException e) {
+            throw new ResumeDeserializationException("Error reading int value", e);
+        }
+    }
+}
