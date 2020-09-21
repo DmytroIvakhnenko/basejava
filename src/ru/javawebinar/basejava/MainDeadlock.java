a4d6f15fbcd61d1741a85dfa7c4ff036ba5aa2ac
Dmytro Ivakhnenko <dim@iowa.usa.com>
2020-09-21 21:47:58 +0300

    Add fix to MainDeadlock

--- src/ru/javawebinar/basejava/MainDeadlock.java
+++ src/ru/javawebinar/basejava/MainDeadlock.java
@@ -4,44 +4,36 @@ public class MainDeadlock {
     private static final Object RESOURCE_1 = new Object();
     private static final Object RESOURCE_2 = new Object();
 
-    public void workWithResource1() {
-        synchronized (RESOURCE_1) {
-            System.out.println(Thread.currentThread().getName() + " locked resource #1");
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            synchronized (RESOURCE_2) {
-                System.out.println(Thread.currentThread().getName() + " locked resource #2");
-            }
-        }
-        System.out.println(Thread.currentThread().getName() + " unlocked resource #1");
+    public void printThreadData(final String status, final Object resource) {
+        System.out.println(Thread.currentThread().getName() + status + resource);
     }
 
-    public void workWithResource2() {
-        synchronized (RESOURCE_2) {
-            System.out.println(Thread.currentThread().getName() + " locked resource #2");
+    public void workWithResource(final Object resource1, final Object resource2) {
+        printThreadData(" waiting ", resource1);
+        synchronized (resource1) {
+            printThreadData(" locked ", resource1);
             try {
                 Thread.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            synchronized (RESOURCE_1) {
-                System.out.println(Thread.currentThread().getName() + " locked resource #1");
+            printThreadData(" waiting ", resource2);
+            synchronized (resource2) {
+                printThreadData(" locked ", resource2);
             }
+            printThreadData(" unlocked ", resource2);
         }
-        System.out.println(Thread.currentThread().getName() + " unlocked resource #2");
+        printThreadData(" unlocked ", resource1);
     }
 
     public static void main(String[] args) {
 
         final MainDeadlock mainDeadlock = new MainDeadlock();
 
-        Thread thread1 = new Thread(mainDeadlock::workWithResource1);
+        Thread thread1 = new Thread(() -> mainDeadlock.workWithResource(RESOURCE_1, RESOURCE_2));
         thread1.start();
 
-        Thread thread2 = new Thread(mainDeadlock::workWithResource2);
+        Thread thread2 = new Thread(() -> mainDeadlock.workWithResource(RESOURCE_2, RESOURCE_1));
         thread2.start();
     }
 
